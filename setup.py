72201b4f302d16e3efa646740378322c4a9e1df9
Megha <megharashokashok@gmail.com>
2025-03-18 16:41:05 +0530

    setup phase is completed

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from setuptools   import find_packages, setup
+from setuptools  import find_packages, setup
+from  typing import List
+hypen_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+   '''
+   defining all requirements 
+   '''
+   requirements=[]
+   with open(file_path) as file_obj:
+     requirements=file_obj.readlines()
+     requirements=[req.replace("\n","") for req in requirements]
+     if hypen_dot in requirements:
+       requirements.remove(hypen_dot)
+   return requirements
+      
 setup(
  name='version',
  version='0.0.1',
  author='Megha',
  author_email="megharashokashok@gmail.com",
  packages=find_packages(),
- install_requires=["pandas","numpy","seaborn"]
+ install_requires=get_requirements('requirements.txt')
 )
