eb3f0e6b2456cbcc04abe72443e1ce80ea664c64
Megha <megharashokashok@gmail.com>
2025-03-18 16:14:59 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools   import find_packages, setup
+setup(
+ name='version',
+ version='0.0.1',
+ author='Megha',
+ author_email="megharashokashok@gmail.com",
+ packages=find_packages(),
+ install_requires=["pandas","numpy","seaborn"]
+)
