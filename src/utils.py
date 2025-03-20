43747d87aeb1bcfc21795375ab668d953f0be9a1
Megha <megharashokashok@gmail.com>
2025-03-20 20:26:45 +0530

    hyperparameter tuning done

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
   try:
@@ -15,19 +16,32 @@ def save_object(file_path,obj):
       
   except Exception as e:
     raise CustomException(e,sys)
-  
 
-def model_evaluator(x_train, y_train, x_test, y_test, models):
-    model_score=[]
-    model_list=[]
-    
-    for key,model in models.items():
-      model.fit(x_train,y_train)
-      train_predicted=model.predict(x_train)
-      test_predicted=model.predict(x_test)
-      
-      train_r2=r2_score(y_train,train_predicted)
-      test_r2=r2_score(y_test,test_predicted)
-      model_list.append(model)
-      model_score.append(test_r2)
-    return model_list, model_score
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        model_list = []
+        model_score = []
+
+        for key, model in models.items():  
+            para = param.get(key, {})  
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            model_list.append(model)
+            model_score.append(test_model_score)
+
+        return model_list, model_score
+
+    except Exception as e:
+        raise CustomException(e, sys)  
