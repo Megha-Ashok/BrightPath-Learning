14ad470da942b7d380ca48518237ee41f48227e7
Megha <megharashokashok@gmail.com>
2025-03-23 00:10:51 +0530

    .ebextensions folder added for entry point of application website deployment process

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from src.logger import logging
+import pickle
 def save_object(file_path,obj):
   try:
     dir_path=os.path.dirname(file_path)
@@ -44,4 +45,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         return model_list, model_score
 
     except Exception as e:
-        raise CustomException(e, sys)  
+        raise CustomException(e, sys) 
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys) 
